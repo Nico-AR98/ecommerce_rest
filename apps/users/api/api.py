c49e5e8e59e45d9c10bda4e7e446073099c0e242
Nico Gallardo <alfredongallg@gmail.com>
2022-10-25 18:49:27 -0300

    Clase 4 - Decorador @API_VIEW

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,13 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from apps.users.api.serializers import UserSerializer
 from apps.users.models import User
 
-class UserAPIView(APIView):
+@api_view(['GET','POST']) #Al decorador api_view le pasamos en forma de lista aquellos métodos que va a permitir
+def user_api_view(request):
 
-    def get(self,request):
-        '''Este método sera el encargado de responder cualquier petición que un front envie por el método HTTP GET'''
+    if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users, many = True) #El atributo 'many = True' se emplea cuando queremos que nos retorne un listado de objetos y no solo un único valor
         return Response(users_serializer.data)
 
+    elif request.method=='POST':
+        #Toda la información que se envie por el método POST estará guardada en request.data
+        print(request.data)
